753445f21536579f8cefd53edc58b53c354c54c1
AbhikS847 <abhikshrestha@gmail.com>
2023-03-10 23:58:02 +1100

    Register route finished for Flight pasa

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
 const app = express();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {console.log(`Server is running on port ${port}`)});
+connectDB();
 
 app.get("/", (req, res) => {
-    res.send("test");
+    res.status(200).json("Welcome to the flight pasa API");
 });
+
+//Routes
+app.use('/flightpasa/api/users', require('./routes/userRoutes'));
+app.use(errorHandler);
+
+app.listen(port, () => {console.log(`Server is running on port ${port}`)});
