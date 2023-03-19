62c07cfbfeb38c0ef1ad8d5428a0d9f08cf65399
AbhikS847 <abhikshrestha@gmail.com>
2023-03-20 00:55:21 +1100

    Created UI for login and signup

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {Container, Form, Button} from 'react-bootstrap';
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+}
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container className="component-theme">
+      <h2 className="pt-3">LOG IN</h2>
+      <div>Welcome back! Your friends are waiting to fly with you :)</div>
+      <Form className="component-form" onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="email">
+      <div className="small">email</div>
+        <Form.Control className="component-form-input" type="email" placeholder="enter email" required/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="password">
+      <div className="small">password</div>
+        <Form.Control className="component-form-input" type="password" placeholder="enter password" required />
+      </Form.Group>
+      <Button className="btn btn-sm" type='submit'>LOG IN</Button>
+      <div className="py-3 text-center">
+        Don't have an account? <a href="/signup">Sign up </a>
+      </div>
+      </Form>
+    </Container>
   )
 }
 
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import {Button, Container, Form} from 'react-bootstrap';
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+}
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <Container className="component-theme">
+      <h2 className="pt-3">SIGN UP</h2>
+      <div>Let's get you started with your Flight Pasa account</div>
+      <Form className="component-form" onSubmit={handleSubmit}>
+      <div className="small">username</div>
+      <Form.Group className="mb-3" controlId="username">
+        <Form.Control className="component-form-input" type="text" placeholder="enter username" required/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="email">
+      <div className="small">email</div>
+        <Form.Control className="component-form-input" type="email" placeholder="enter email" required/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="password">
+      <div className="small">password</div>
+        <Form.Control className="component-form-input" type="password" placeholder="enter password" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="password">
+      <div className="small">Confirm password</div>
+        <Form.Control className="component-form-input" type="password" placeholder="enter password again" required />
+      </Form.Group>
+      <Button className="btn btn-sm" type='submit'>SIGN UP</Button>
+      <div className="py-3 text-center">
+        Already have an account? <a href="/login">Login</a>
+      </div>
+      </Form>
+    </Container>
   )
 }
 
